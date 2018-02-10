aab55e17f27ab5356f36cc7f8d0bb8d91769cde9
JasperDre <jasperdre@gmail.com>
2018-02-10 16:08:27 +0100

    [ADDED] Scriptable planets and UI behaviour

---
+++ Prototype/Assets/Scripts/UI/PlanetDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetDisplay : MonoBehaviour
+{
+    public Text planetName;
+    public Text planetGalaxy;
+    public Text planetDistance;
+    public Text planetDiameter;
+    public Text planetDescription;
+    public Image planetImage;
+    public Image planetBackground;
+    public PlanetObject initialPlanet;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ChangeActivePlanet(initialPlanet);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ChangeActivePlanet(PlanetObject planet)
+    {
+        planetName.text = planet.planetName;
+        planetGalaxy.text = planet.planetGalaxy + " galaxy";
+        planetDistance.text = planet.planetDistance.ToString() + " km";
+        planetDiameter.text = planet.planetDiameter.ToString() + " km";
+        planetDescription.text = planet.planetDescription;
+        planetImage.sprite = planet.planetImage;
+        planetBackground.sprite = planet.planetBackground;
+    }
+}
---
+++ Prototype/Assets/Scripts/UI/PlanetObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Planet", menuName = "Planet")]
+public class PlanetObject : ScriptableObject
+{
+    public string planetName;
+    public string planetGalaxy;
+    public float planetDistance;
+    public float planetDiameter;
+    public string planetDescription;
+    public Sprite planetImage;
+    public Sprite planetBackground;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
