d5a627ad3939e21903732d9025d68df691609454
JasperDre <jasperdre@gmail.com>
2018-02-17 18:18:59 +0100

    [ADDED] UI sprites

---
+++ Prototype/Assets/Scripts/UI/FuelObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Fuel", menuName = "Fuel")]
+public class FuelObject : ScriptableObject
+{
+    public string fuelName;
+    public string fuelDescription;
+    public float fuelLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Prototype/Assets/Scripts/UI/FuelSelection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelSelection : MonoBehaviour
+{
+    public Text Name;
+    public Text DescriptionText;
+
+    // Use this for initialization
+    void Start()
+    {
+        Name.text = "";
+        DescriptionText.text = "Choose one of the fuel types as your rocket propellant.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeFuelType(FuelObject fuel)
+    {
+        PlayerPrefs.SetString("FuelType", fuel.fuelName);
+        Name.text = fuel.fuelName;
+        DescriptionText.text = fuel.fuelDescription;
+    }
+}
