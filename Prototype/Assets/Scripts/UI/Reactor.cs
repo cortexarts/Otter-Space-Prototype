b2d9a53fb3df7b8113e61ceb714788074caa4268
JasperDre <jasperdre@gmail.com>
2018-03-10 19:40:43 +0100

    [ADDED] Font and reactions

--- Prototype/Assets/Scripts/UI/Reactor.cs
+++ Prototype/Assets/Scripts/UI/Reactor.cs
@@ -8,6 +8,11 @@ using UnityEngine.Events;
 public class Reactor : MonoBehaviour
 {
     public List<FuelObject> fuelObjects;
+    public Button LiquidHydrogen;
+    public Button Methane;
+    public Button Hydrazine;
+    public Button LiquidOxygen;
+    public Button Coal;
     public List<Button> buttons;
     public List<string> names;
 
@@ -18,7 +23,6 @@ public class Reactor : MonoBehaviour
 
     public void BeginDrag(GameObject target)
     {
-        Debug.Log("Started drag");
         offsetX = target.transform.position.x - Input.mousePosition.x;
         offsetY = target.transform.position.y - Input.mousePosition.y;
     }
@@ -30,8 +34,6 @@ public class Reactor : MonoBehaviour
 
     public void EndDrag()
     {
-
-
         for (int i = 0; i < buttons.Count - 1; i++)
         {
             for (int j = 1; j < buttons.Count; j++)
@@ -39,7 +41,6 @@ public class Reactor : MonoBehaviour
                 float distance = Vector2.Distance(buttons[i].transform.position, buttons[j].transform.position);
                 if (distance < maxDistance)
                 {
-                    Debug.Log("Overlapping!");
                     for (int f = 0; f < fuelObjects.Count; f++)
                     {
                         for (int e = 0; e < fuelObjects[f].elements.Count; e++)
@@ -48,7 +49,36 @@ public class Reactor : MonoBehaviour
                             {
                                 if (fuelObjects[f].elements[e] == names[e])
                                 {
-                                    Debug.Log("Reaction is possible!");
+                                    switch (fuelObjects[f].elements[e])
+                                    {
+                                        case "Hydrogen":
+                                            buttons[i].gameObject.SetActive(false);
+                                            buttons[j].gameObject.SetActive(false);
+                                            LiquidHydrogen.transform.position = buttons[i].transform.position;
+                                            LiquidHydrogen.gameObject.SetActive(true);
+                                            break;
+                                        case "Nitrogen":
+                                            buttons[i].gameObject.SetActive(false);
+                                            buttons[j].gameObject.SetActive(false);
+                                            Hydrazine.transform.position = buttons[i].transform.position;
+                                            Hydrazine.gameObject.SetActive(true);
+                                            break;
+                                        case "Oxygen":
+                                            buttons[i].gameObject.SetActive(false);
+                                            buttons[j].gameObject.SetActive(false);
+                                            LiquidOxygen.transform.position = buttons[i].transform.position;
+                                            LiquidOxygen.gameObject.SetActive(true);
+                                            break;
+                                        case "Carbon":
+                                            buttons[i].gameObject.SetActive(false);
+                                            buttons[j].gameObject.SetActive(false);
+                                            Coal.transform.position = buttons[i].transform.position;
+                                            Coal.gameObject.SetActive(true);
+                                            break;
+                                        default:
+                                            Debug.LogWarning("No reaction results!");
+                                            break;
+                                    }
                                 }
                             }
                         }
