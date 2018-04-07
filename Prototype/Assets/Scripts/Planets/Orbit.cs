1bfeb81ac90b12b3a102f208979ed1a0c8901f13
JasperDre <jasperdre@gmail.com>
2018-04-07 13:39:06 +0200

    [ADDED] Moon

---
+++ Prototype/Assets/Scripts/Planets/Orbit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
+    void Start()
+    {
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        Vector3 desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
