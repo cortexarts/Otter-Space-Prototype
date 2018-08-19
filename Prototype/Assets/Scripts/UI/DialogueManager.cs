221a0a461172a23c91bafa88460d511ec95b6d13
Jasper Drescher <jasperdre@gmail.com>
2018-08-19 21:50:00 +0200

    [ADDED] Dialogue to intro

---
+++ Prototype/Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+public struct Phrase
+{
+    public string character;
+    public string text;
+}
+
+public struct Dialogue
+{
+    public int index;
+    public List<Phrase> phrases;
+}
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text m_Name;
+    public Text m_Text;
+    public TextAsset source;
+    public float letterPause = 0.05f;
+    public float DialoguePause = 0.1f;
+    public int dialogueIndex = 0;
+    private int phraseIndex = 0;
+    private string message = "";
+
+    public List<Dialogue> dialogues = new List<Dialogue>();
+
+    private IntroCanvasManager m_IntroCanvasManager;
+
+    void Start()
+    {
+        ParseDialogue();
+        m_IntroCanvasManager = GetComponent<IntroCanvasManager>();
+    }
+
+    public void ParseDialogue()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(source.text);
+        XmlNodeList dialogueList = xmlDoc.GetElementsByTagName("dialogue");
+
+        foreach(XmlNode dialogueInfo in dialogueList)
+        {
+            XmlNodeList dialogueContent = dialogueInfo.ChildNodes;
+            Dialogue currentXMLDialogue;
+            currentXMLDialogue.phrases = new List<Phrase>();
+            int.TryParse(dialogueInfo.Attributes["index"].Value, out currentXMLDialogue.index);
+
+            foreach(XmlNode dialogueItems in dialogueContent)
+            {
+                if(dialogueItems.Name == "phrase")
+                {
+                    Phrase currentXMLPhrase;
+                    currentXMLPhrase.character = dialogueItems.Attributes["name"].Value;
+                    currentXMLPhrase.text = dialogueItems.InnerText;
+                    currentXMLDialogue.phrases.Add(currentXMLPhrase);
+                }
+            }
+
+            dialogues.Add(currentXMLDialogue);
+        }
+    }
+
+    public void CreateDialogue()
+    {
+        Phrase currentPhrase;
+        currentPhrase = dialogues[dialogueIndex].phrases[phraseIndex];
+        message = currentPhrase.text;
+        m_Name.text = currentPhrase.character;
+    }
+
+    public void PlayDialogue(int a_Index)
+    {
+        dialogueIndex = a_Index;
+        m_Name.text = "";
+        m_Text.text = "";
+        CreateDialogue();
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach(char letter in message.ToCharArray())
+        {
+            m_Text.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+
+        if(phraseIndex < dialogues[dialogueIndex].phrases.Count - 1)
+        {
+            yield return new WaitForSeconds(DialoguePause);
+            phraseIndex++;
+            PlayDialogue(dialogueIndex);
+        }
+        else
+        {
+            yield return new WaitForSeconds(DialoguePause);
+            m_IntroCanvasManager.ToNextState();
+        }
+    }
+}
--- Prototype/Assets/Scripts/UI/IntroCanvasManager.cs
+++ Prototype/Assets/Scripts/UI/IntroCanvasManager.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class IntroCanvasManager : MonoBehaviour
 {
-    public enum State { Default, Controls, Animation, Crafting, Reading, Playing };
+    public enum State { Default, Controls, Animation, Crafting, Notebook, Dialogue, Playing };
     public State currentState;
     public GameObject panelControls;
     public GameObject panelAnimation;
     public GameObject panelHUD;
     public GameObject panelLab;
     public GameObject panelNotebook;
+    public GameObject panelDialogue;
 
     private CameraController cameraController;
+    private DialogueManager m_DialogueManager;
 
     // Use this for initialization
     void Start ()
     {
         cameraController = FindObjectOfType<CameraController>();
+        m_DialogueManager = GetComponent<DialogueManager>();
         currentState = State.Default;
         ToNextState();
 	}
@@ -45,6 +48,7 @@ public class IntroCanvasManager : MonoBehaviour
                 panelHUD.SetActive(false);
                 panelLab.SetActive(false);
                 panelNotebook.SetActive(false);
+                panelDialogue.SetActive(false);
                 cameraController.isZooming = false;
                 currentState = State.Controls;
                 break;
@@ -73,14 +77,26 @@ public class IntroCanvasManager : MonoBehaviour
                 panelLab.SetActive(false);
                 panelNotebook.SetActive(true);
                 cameraController.isZooming = false;
-                currentState = State.Reading;
+                currentState = State.Notebook;
                 break;
-            case State.Reading:
+            case State.Notebook:
                 panelControls.SetActive(false);
                 panelAnimation.SetActive(false);
-                panelHUD.SetActive(true);
+                panelHUD.SetActive(false);
                 panelLab.SetActive(false);
                 panelNotebook.SetActive(false);
+                panelDialogue.SetActive(true);
+                cameraController.isZooming = false;
+                m_DialogueManager.PlayDialogue(1);
+                currentState = State.Dialogue;
+                break;
+            case State.Dialogue:
+                panelControls.SetActive(false);
+                panelAnimation.SetActive(false);
+                panelHUD.SetActive(false);
+                panelLab.SetActive(false);
+                panelNotebook.SetActive(false);
+                panelDialogue.SetActive(false);
                 cameraController.isZooming = true;
                 currentState = State.Playing;
                 break;
@@ -89,6 +105,8 @@ public class IntroCanvasManager : MonoBehaviour
                 panelAnimation.SetActive(false);
                 panelHUD.SetActive(true);
                 panelLab.SetActive(false);
+                panelDialogue.SetActive(false);
+                panelNotebook.SetActive(false);
                 cameraController.isZooming = true;
                 currentState = State.Playing;
                 break;
